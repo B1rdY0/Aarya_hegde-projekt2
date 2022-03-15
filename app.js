7aa272c54e9125da3713fcc55c646bf4345ead67
Aaryan hegde <hegde.aaryan@gmail.com>
2022-03-15 23:28:56 +0100

    Shop

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
+
+
 let clicked = 0;
 
 let button = document.querySelector(".Sugarbuy")
-console.log(button);
 
-button.addEventListener("click", handleClick)
+button.addEventListener("click", handleClick1)
+
+function handleClick1() {
+    clicked++;
+    button.innerHTML = `You have: ${clicked}`;
+
+    if(clicked > 2){
+        clicked = -1;
+    }
+    updateFields();
+
+}
+
+
+
+
+
+let clicked1 = 0;
+
+let buttonRed = document.querySelector(".RedBull")
+
+buttonRed.addEventListener("click", handleClick)
 
 function handleClick() {
-    clicked++;
-    button.innerHTML = `You have: ${clicked}`; x
-
+    clicked1++;
+    buttonRed.innerHTML = `You have: ${clicked1}`;
+    if(clicked1 > 2){
+        clicked1 = -1;
+    }
+    updateFields();
 }
 
-if (clicked < 3) {
-    clicked = 0;
+
+
+
+
+let clicked2 = 0;
+
+let buttonWhite = document.querySelector(".Cocain")
+
+buttonWhite.addEventListener("click", handleClick2)
+
+function handleClick2(){
+    clicked2++;
+    buttonWhite.innerHTML = `You have: ${clicked2}`; 
+    if(clicked2   > 2){
+        clicked2 = -1; 
+    }
+    updateFields();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
